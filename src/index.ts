8152d746c8d3a6b5feef23e7496145b3d42fc996
Lauren Coates <lspassey@gmail.com>
2024-08-24 16:57:30 -0700

    fixed error in schema and adds function for menu

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,63 @@
 import inquirer from "inquirer";
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 import pg from "pg";
 
-const { Pool } = pg; 
+const { Pool } = pg;
 const pool = new Pool({
-    host: "localhost",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 5432,
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
 });
 const client = await pool.connect();
 
-function createDepartment() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "name",
-                message: "What is the name of the department?",
-            },
-        ])
-        .then((answers) => {
-            client.query(
-                `INSERT INTO department (name) VALUES ('${answers.name}')`
-            );
-        });
-        
+function menu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "What would you like to do?",
+        choices: [
+          "Add Employee",
+          "Add Role",
+          "Add Department",
+          "Update Employee Role",
+          "View Employees",
+          "View Roles",
+          "View Departments",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.menu) {
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+        case "View Employees":
+          viewEmployees();
+          break;
+        case "View Roles":
+          viewRoles();
+          break;
+        case "View Departments":
+          viewDepartments();
+          break;
+        case "Exit":
+          pool.end();
+          process.exit();
+          break;
+      }
+    });
 }
-
