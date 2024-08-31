56aa92da8c05baadbc03823b6996031d5fb5456d
Lauren Coates <lspassey@gmail.com>
2024-08-31 08:32:43 -0700

    fixed error in schema.sql

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,25 +1,27 @@
-DROP DATABASE IF EXSISTS 'EMPLOYEE_DB';
-CREATE DATABASE 'EMPLOYEE_DB';
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-CREATE TABLE 'department' (
-    'id' SERIAL PRIMARY KEY,
-    'name' VARCHAR(30) UNIQUE NOT NULL
+\c employee_db;
+
+CREATE TABLE "department" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE 'role' (
-    'id' SERIAL PRIMARY KEY,
-    'title' VARCHAR(30) UNIQUE NOT NULL,
-    'salary' DECIMAL NOT NULL
-    'department_id' VARCHAR(30) UNIQUE NOT NULL,
-    references: 'department'('id')
+CREATE TABLE "role" (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE 'employee' (
-    'id' SERIAL PRIMARY KEY,
-    'first_name' VARCHAR(30) NOT NULL,
-    'last_name' VARCHAR(30) NOT NULL,
-    'role_id' INTEGER NOT NULL,
-    references: 'role'('id'),
-    'manager_id' INTEGER,
-    references: 'employee'('id')
+CREATE TABLE "employee" (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
     );
---
+++ src/db/seeds.sql
@@ -0,0 +1,2 @@
+\c employee_db;
+
