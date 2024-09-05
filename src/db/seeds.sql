e26d05d6ab4816c73ef1a5ce4f47a0f695388b4d
Lauren Coates <lspassey@gmail.com>
2024-09-04 21:50:17 -0700

    fixed error in schema.sql

--- src/db/seeds.sql
+++ src/db/seeds.sql
@@ -14,7 +14,7 @@ INSERT INTO role (title, salary, department_id) VALUES
 ('Sales Person', 70000, 2),
 ('Lawyer', 90000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+INSERT INTO employee (first_name, last_name, role_id, salary, manager_id) VALUES 
 ('Alice', 'Johnson', 1, NULL),
 ('Bob', 'Smith', 2, 1), 
 ('Charlie', 'Thompson', 3, 1),  
